b87329adddba4eaed8d3e4f3befdda0934acf7b0
aramellanyu <123909581+aramellanyu@users.noreply.github.com>
2023-02-15 19:46:56 -0500

    Add files via upload

--- webServer.py
+++ webServer.py
@@ -10,7 +10,6 @@ def webServer(port=13331):
     # Prepare a server socket
     serverSocket.bind(("", port))
 
-
     # Fill in start
     serverSocket.listen(1)
     # Fill in end
@@ -18,47 +17,48 @@ def webServer(port=13331):
     while True:
         # Establish the connection
 
-       # print('Ready to serve...')
-        connectionSocket, addr =  serverSocket.accept() #Fill in end
+        print('Ready to serve...')
+        connectionSocket, addr =  serverSocket.accept()   #Fill in end
 
         try:
-            message = connectionSocket.recv(1024)   #Fill in end
+            message =  connectionSocket.recv(1024).decode()   #Fill in end
             filename = message.split()[1]
 
             # opens the client requested file.
             # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-            f = open(filename[1:], )
-                     # fill in end
+            f = open(filename[1:], 'r', message.length)
+                     #fill in end
 
-            outputdata=b"Content-Type: text/html; charset=UTF-8\r\n"
+            #outputdata=b"Content-Type: text/html; charset=UTF-8\r\n"
             # Fill in start -This variable can store your headers you want to send for any valid or invalid request.
+            outputdata=connectionSocket.send(b"")
+
             # Content-Type above is an example on how to send a header as bytes
-
-
             # Fill in end
 
             # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
+            outputdata= b"'\nHTTP/1.1 200 OK\r\n\r\n"
             # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
             # Fill in start
-            connectionSocket.send('\nHTTP/1.1 200 OK \r\n\r\n')
+
             # Fill in end
 
             # Send the content of the requested file to the client
             for i in f:  # for line in file
-                connectionSocket.send(outputdata[i])
-
-            connectionSocket.close()  # closing the connection socket
+            # Fill in start - send your html file contents #Fill in end
+                connectionSocket.send(outputdata[i].encode())
+                connectionSocket.send("\r\n".encode())
+                connectionSocket.close()  # closing the connection socket
 
         except Exception as e:
-
     # Send response message for invalid request due to the file not being found (404)
     # Fill in start
-   # print("404 Page Not Found")
-            connectionSocket.send('\nHTTP/1.1 404 Not Found\r\n\r\n')
+           outputdata=b"\nHTTP/1.1 404 Not Found\r\n\r\n"
     # Fill in end
 
     # Close client socket
     # Fill in start
+        connectionSocket.close()
 
     # Fill in end
 
