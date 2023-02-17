9bf828fac0612609b830b1d1654b0772f4c5d51f
aramellanyu <123909581+aramellanyu@users.noreply.github.com>
2023-02-16 21:07:25 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -40,7 +40,7 @@ def webServer(port=13331):
             # Fill in start -This variable can store your headers you want to send for any valid or invalid request.
 
             # Content-Type above is an example on how to send a header as bytes
-            connectionSocket.sendall(outputdata)
+        
             # Fill in end
 
             # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
