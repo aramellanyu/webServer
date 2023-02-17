687225a564e76455d52e4bb3f9b2a9205dac425c
aramellanyu <123909581+aramellanyu@users.noreply.github.com>
2023-02-16 20:58:59 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -23,7 +23,7 @@ def webServer(port=13331):
         connectionSocket, addr = serverSocket.accept()
 
         try:
-            message = connectionSocket.recv(1024)
+            message = connectionSocket.recv(1024).decode()
 
             filename = message.split()[1]
 
@@ -33,8 +33,6 @@ def webServer(port=13331):
             # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
             f = open(filename[1:] ,'r')
             line = f.readlines()
-          
-
 
             # fill in end
 
