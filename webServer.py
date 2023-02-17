e21770574101ada917b16cda9e1d2b172035762b
aramellanyu <123909581+aramellanyu@users.noreply.github.com>
2023-02-16 21:09:15 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -23,7 +23,7 @@ def webServer(port=13331):
         connectionSocket, addr = serverSocket.accept()
 
         try:
-            message = connectionSocket.recv(1024)
+            message = connectionSocket.recv(1024).decode()
 
             filename = message.split()[1]
 
@@ -32,7 +32,7 @@ def webServer(port=13331):
             # opens the client requested file.
             # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
             f = open(filename[1:] ,'r')
-            line = f.readlines(50)
+            line = f.readlines()
 
             # fill in end
 
@@ -46,7 +46,7 @@ def webServer(port=13331):
             # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
             # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
             # Fill in start
-            connectionSocket.send('\nHTTP/1.1 200 OK\r\n\r\n'.encode())
+            connectionSocket.send('\HTTP/1.1 200 OK\r\n\r\n'.encode())
             # Fill in end
 
             # Send the content of the requested file to the client
@@ -57,7 +57,7 @@ def webServer(port=13331):
                 connectionSocket.close() 
 
         except Exception as e:
-            connectionSocket.send("\nHTTP/1.1 404 Not Found\r\n\r\n".encode())
+            connectionSocket.send("\HTTP/1.1 404 Not Found\r\n\r\n".encode())
             connectionSocket.close()
 
 # Send response message for invalid request due to the file not being found (404)
