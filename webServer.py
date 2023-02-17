6e5ea5607db7d8ea2029823660f438789c93088f
aramellanyu <123909581+aramellanyu@users.noreply.github.com>
2023-02-16 21:02:47 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -23,7 +23,7 @@ def webServer(port=13331):
         connectionSocket, addr = serverSocket.accept()
 
         try:
-            message = connectionSocket.recv(1024).decode()
+            message = connectionSocket.recv(1024)
 
             filename = message.split()[1]
 
